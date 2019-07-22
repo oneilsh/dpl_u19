c7e2da6553063c1a8e94dfdfaa07e9f4b5c2fb23
Shawn O'Neil <oneilsh@gmail.com>
2019-07-22 21:59:25 +0000

    example 4-class convnet for imagenet data

--- imagenet_loading.R
+++ imagenet_loading.R
@@ -1,6 +1,9 @@
 library(keras)
 library(tensortree)
 
+# remove images anywhere within the folder imagenet_validation that aren't of type JPG
+# find imagenet_validation -type f -exec file --mime-type {}  \; | awk '{if($NF!= "image/jpeg") print $0}' | awk -F: '{print "\""$1"\""}'
+
 ## generators for training data
 train_datagen <- image_data_generator(rescale = 1/255)
 
@@ -30,7 +33,25 @@ validate_generator <- flow_images_from_directory("datasets/imagenet_validation",
                                                  classes = c("huntsman", "beetle", "centipede", "shiitake"))
 
 
-# example fitting (if network has the right input and output shape, and has been compiled...)
+network <- keras_model_sequential() %>%
+  layer_conv_2d(filters = 16, kernel_size = c(3, 3), activation = "relu", input_shape = c(100, 100, 3)) %>%
+  layer_max_pooling_2d(pool_size = c(2, 2)) %>%   # strides defaults to pool_size
+  
+  layer_conv_2d(filters = 32, kernel_size = c(3, 3), activation = "relu") %>%
+  layer_max_pooling_2d(pool_size = c(2, 2)) %>%
+  
+  layer_conv_2d(filters = 64, kernel_size = c(3, 3), activation = "relu") %>%
+  layer_max_pooling_2d(pool_size = c(2, 2)) %>%  
+  
+  layer_flatten() %>%
+  layer_dense(units = 32, activation = "relu") %>%
+  layer_dense(units = 4, activation = "softmax")
+
+compile(network, 
+        optimizer = "rmsprop",
+        loss = "categorical_crossentropy",
+        metrics = c("accuracy"))
+
 
 history <- fit_generator(network, 
                          train_generator,
@@ -38,4 +59,6 @@ history <- fit_generator(network,
                          steps_per_epoch = 130,
                          epochs = 10,
                          validation_steps = 50)
+
+save_model_hdf5(network, "insects_mushroom_network_v1.h5")
 
